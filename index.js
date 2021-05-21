366ca9b37274dffef51422e72676519f2ee09291
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-21 11:07:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,31 @@ function saturdayFun(activity="roller-skate") {
 let mondayWork = function(activity="go to the office") {
   return `This Monday, I will ${activity}.`;
 }
+function wrapAdjective(flair="*") {
+  return function(adj="special") {
+    return `You are ${flair }${adj}${flair}!`;
+  }
+}
+
+const Calculator = {
+  add: function(a, b) {
+    return a + b;
+  },
+  subtract: function(a, b) {
+    return a - b;
+  },
+  multiply: function(a, b) {
+    return a * b;
+  },
+  divide: function(a, b) {
+    return a / b;
+  }
+}
+
+function actionApplyer(starting, arrOfFunc) {
+   for (let i = 0; i < arrOfFunc.length; i++) {
+    starting = arrOfFunc[i](starting);
+  }
+  return starting;
+
+} 
