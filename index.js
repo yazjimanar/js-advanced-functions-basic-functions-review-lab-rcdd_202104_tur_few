e21397c5175f0e927a4c0a5b82301982fc0cbb0d
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-21 11:06:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`;
 }
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
